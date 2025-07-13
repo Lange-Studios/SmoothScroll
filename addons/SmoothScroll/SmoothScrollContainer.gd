2864e7bce9a614a42b09544a83eccae78abac9b1
Travis Lange <travislange12@gmail.com>
2025-07-13 15:36:19 -0400

    fix compiler error in smooth scroll

--- addons/SmoothScroll/SmoothScrollContainer.gd
+++ addons/SmoothScroll/SmoothScrollContainer.gd
@@ -269,7 +269,7 @@ func _gui_input(event: InputEvent) -> void:
 	if (event is InputEventScreenDrag and drag_with_touch) \
 			or (event is InputEventMouseMotion and drag_with_mouse):
 		if content_dragging:
-			OS.has_feature("mobile"):
+			if OS.has_feature("mobile"):
 				event.relative.x /= 2
 				event.relative.y /= 2
 			if should_scroll_horizontal():
